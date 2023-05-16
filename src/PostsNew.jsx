a0363e6d4c6321c1585093e6b2bccd35253c7375
Sam Wells <sfwells0518@gmail.com>
2023-05-16 14:11:19 -0500

    successfully test new post creation

--- src/PostsNew.jsx
+++ src/PostsNew.jsx
@@ -1,26 +1,35 @@
+import axios from "axios";
+
 export function PostsNew() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    axios.post("http://localhost:3000/posts.json", params).then(response => {
+      console.log(response.data);
+      event.target.reset()
+    })
+  }
+
   return (
     <div id="posts-new" className="centered-content">
       <h3>New Post</h3>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-floating mb-3">
-          <input type="text" className="form-control" id="floatingTitle" placeholder="Title" />
+          <input name="title" type="text" className="form-control" id="floatingTitle" placeholder="Title" />
           <label htmlFor="floatingInput">Title</label>
         </div>
 
         <div className="form-floating mb-3">
-          <input type="text" className="form-control form-control-large" id="floatingBody" placeholder="Body" />
+          <input name="body" type="text" className="form-control form-control-large" id="floatingBody" placeholder="Body" />
           <label htmlFor="floatingInput">Body</label>
         </div>
 
         <div className="form-floating mb-3">
-          <input type="text" className="form-control" id="floatingImage" placeholder="Image" />
+          <input name="image" type="text" className="form-control" id="floatingImage" placeholder="Image" />
           <label htmlFor="floatingInput">Image URL</label>
         </div>
 
-        <br></br>
-
-        <button type="submit">Create Post</button>
+        <button type="submit">Submit Post</button>
       </form>
       <br></br>
     </div>
