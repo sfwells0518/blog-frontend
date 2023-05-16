7d4f1996990ab87fa812ff4cf925a61b9eb3376d
Sam Wells <sfwells0518@gmail.com>
2023-05-16 12:31:52 -0500

    update signup code from html to js

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@ export function Header() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#">Sam's Blog</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,8 +1,18 @@
+import axios from "axios";
+
 export function Signup() {
+  const handleSubmit = (event) => {
+    console.log("handling submit");
+    event.preventDefault()
+    const params = new FormData(event.target)
+    axios.post("http://localhost:3000/users.json", params).then((response) => {
+      console.log(response.data);
+    });
+  };
   return (
     <div id="signup">
       <h3>Sign Up</h3>
-      <form action="http://localhost:3000/users.json" method="POST">
+      <form onSubmit={handleSubmit}>
         <div>
           Name: <input name="name" type="text" />
         </div>
